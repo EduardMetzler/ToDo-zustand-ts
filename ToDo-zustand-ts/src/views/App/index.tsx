58fd456378d95388e6c48e9be0c85135fceb0cab
Eduard Metzler <eduard.metzler@mail.ru>
2023-07-25 20:43:35 +0200

    added store

--- ToDo-zustand-ts/src/views/App/index.tsx
+++ ToDo-zustand-ts/src/views/App/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useToDoStore } from "../../data/stores/useToDoStore";
+import styles from "./index.module.scss";
 
 export const App: React.FC = () => {
-  return <article>dedewde</article>;
+  const [tasks, createTask, updateTask, removeTask] = useToDoStore((state) => [
+    state.tasks,
+    state.createTask,
+    state.updateTask,
+    state.removeTask,
+  ]);
+  //   createTask("ssssssssssssssssssss");
+  console.log(tasks);
+  return (
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>To Do App</h1>
+      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}></section>
+    </article>
+  );
 };
