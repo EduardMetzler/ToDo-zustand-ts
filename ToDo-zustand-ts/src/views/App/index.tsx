47deca4a5aef9b1d7b8bd37ada867b3f7221b1c5
Eduard Metzler <eduard.metzler@mail.ru>
2023-07-26 23:09:07 +0200

    added inputPlus

--- ToDo-zustand-ts/src/views/App/index.tsx
+++ ToDo-zustand-ts/src/views/App/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useToDoStore } from "../../data/stores/useToDoStore";
 import styles from "./index.module.scss";
+import { InputPlus } from "../components/InputPlus/index";
 
 export const App: React.FC = () => {
   const [tasks, createTask, updateTask, removeTask] = useToDoStore((state) => [
@@ -9,12 +10,20 @@ export const App: React.FC = () => {
     state.updateTask,
     state.removeTask,
   ]);
-  //   createTask("ssssssssssssssssssss");
+
   console.log(tasks);
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
-      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}>
+        <InputPlus
+          onAdd={(title: string) => {
+            if (title) {
+              createTask(title);
+            }
+          }}
+        />
+      </section>
       <section className={styles.articleSection}></section>
     </article>
   );
